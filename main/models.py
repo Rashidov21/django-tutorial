702bd2c90b1be575ec2a9f4448e433fa47e4cba8
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2021-05-20 19:05:28 +0500

    Contact model
    Contact model created

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+	name = models.CharField('Name', max_length=50)# BARCHA BELGILARNI QABUL QILADI
+	email = models.EmailField('Email', max_length=250)# FAQAT EMAIL QABUL QILADI
+	subject = models.CharField('Subject', max_length=150) 
+	message = models.TextField('Message') # KO'P QATORLI MATNLARNI KIRITISH MUMKIN
+
+	def __str__(self):
+		return f"Contacted > {self.name}"
--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import Contact
 # Create your views here.
 
 
@@ -10,4 +10,12 @@ def aboutPage(request):
 	return render(request, 'about.html')
 
 def contactPage(request):
+	
+	if request.method == 'POST':
+		name = request.POST['name']
+		email = request.POST['email']
+		subject = request.POST['subject']
+		message = request.POST['message']
+		print(name,email,subject,message)
+		Contact.objects.
 	return render(request, 'contact.html')
