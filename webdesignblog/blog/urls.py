0caebd1d03ad93f7d549a0fc9e4fd3e00f614598
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-04-09 18:51:49 +0500

    search

--- webdesignblog/blog/urls.py
+++ webdesignblog/blog/urls.py
@@ -5,6 +5,8 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('search/', views.inlineSearch, name='search'),
     path('detail/<slug:slug>', views.PostDetailView.as_view(), name='detail'),
+    path("category/<slug:category_slug>/", views.category_list, name='category_list')
 ]
 # paths    
--- webdesignblog/blog/views.py
+++ webdesignblog/blog/views.py
@@ -1,7 +1,9 @@
+import json
 from django.shortcuts import render
-from .models import Post
-# from django.views.generic.base import TemplateView
+from django.core.paginator import Paginator
 from django.views.generic import ListView,DetailView
+from django.http import JsonResponse
+from .models import Post, Category
 # Create your views here.
 class HomeView(ListView):
     model = Post
@@ -10,6 +12,21 @@ class HomeView(ListView):
 class PostDetailView(DetailView):
     model = Post
 
+def category_list(request, category_slug):
+    category = Category.objects.get(slug=category_slug) #javascript
+    posts = Post.objects.filter(category=category)
+    paginator = Paginator(posts, 2) # Show 2 posts per page.
 
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, "blog/post_list.html",{"page_obj":page_obj})
 # views  
-    
+
+
+def inlineSearch(request):
+    d = json.loads(request.GET.get("data"))
+    queryset = list(Post.objects.filter(title__icontains=d).values())
+    data = {}
+    data["object_list"] = queryset
+
+    return JsonResponse(data)
