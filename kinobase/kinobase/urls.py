7fabeb24c120e86da3f7748c17bbad8742b84037
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-05-07 19:05:14 +0500

    update project

---
+++ kinobase/kinobase/urls.py
@@ -0,0 +1,14 @@
+
+from django.contrib import admin
+from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", include('movie.urls', namespace='movie'))
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,  document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,  document_root=settings.MEDIA_ROOT)
