8e200539c0b3d8758656a8bef90ba5496ac117dc
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2023-04-10 17:31:22 +0500

    todo update

--- todochi/todo/urls.py
+++ todochi/todo/urls.py
@@ -6,4 +6,9 @@ app_name = 'todo'
 urlpatterns = [
     path('', views.TodoView.as_view(), name='home'),
     path("add/", views.AddNewTodo.as_view(), name='add'),
+    path("detail/<pk>", views.TodoDetailView.as_view(), name='detail'),
+    
+    path("delete/<int:todo_id>", views.TodoDeleteView.as_view(), name='delete'),
+    path("update/<pk>", views.TodoUpdateView.as_view(), name='update'),
+    path("done/<int:todo_id>", views.TodoDoneView.as_view(), name='done')
 ]
--- todochi/todo/views.py
+++ todochi/todo/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
-from django.views.generic.edit import CreateView 
+import datetime
+from django.shortcuts import render,redirect
+from django.views.generic import ListView, DetailView
+from django.views.generic.base import View
+from django.views.generic.edit import CreateView , DeleteView, UpdateView
 
 
 
@@ -9,13 +11,74 @@ from .forms import AddNewTodoForm
 from .models import Todo
 
 
-class TodoView(TemplateView):
+class TodoView(ListView):
+    model = Todo
     template_name = 'index.html'
-    extra_context = {"form":AddNewTodoForm()}
+    
+    # def get_queryset(self):
+    #     queryset = Todo.objects.filter(done=False)
+    #     return queryset
+    
+    def get_context_data(self, **kwargs):
+        
+        context = super().get_context_data(**kwargs)
+        context["doned_false"] = Todo.objects.filter(done=False) 
+        context["doned_true"] = Todo.objects.filter(done=True) 
+        return context
+    
+    # extra_context = {"form":AddNewTodoForm()}
 
+class TodoDetailView(DetailView):
+    model = Todo
 
-
+# class TodoDeleteView(DeleteView):
+#     model = Todo
+#     success_url = '/'
+class TodoDeleteView(View):
+    
+    def get(self, request,todo_id, *args, **kwargs):
+        todo = Todo.objects.get(pk=todo_id)
+        todo.delete()
+        return redirect("/")
+    
+class TodoDoneView(View):
+    
+    def get(self, request, todo_id,*args, **kwargs):
+        print(self.request.path)
+        todo = Todo.objects.get(pk=todo_id)
+        todo.done = True
+        todo.doned_at = datetime.datetime.now()
+        todo.save()
+        return redirect("/")
+         
+    
 class AddNewTodo(CreateView):
     model = Todo
     form_class = AddNewTodoForm
     success_url = '/'
+    extra_context = {"form_status":"add"}
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    form_class = AddNewTodoForm
+    success_url = '/'
+    extra_context = {"form_status":"update"}
+    
+    
+    
+# CRUD
+ 
+    # View - http method lariga javob beruvchi ota controller class 
+    
+    
+    
+    # Create - yozish
+    # Retrieve - oqish 
+    # Update  - yangilash 
+    # Delete  - ochirish
+    
+    
+
+    # ListView
+    # DetailView
+    # TemplateView
